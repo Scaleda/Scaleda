edc3d347fc767e643e81fcdb9549f6b7e4b34ceb
criwits <hanswan@tom.com>
2022-12-22 21:12:24 +0800

    something

---
+++ src/main/scala/top/criwits/scaleda/config/EDASettingsPanel.java
@@ -0,0 +1,11 @@
+package top.criwits.scaleda.config;
+
+import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+
+import javax.swing.*;
+
+public class EDASettingsPanel {
+    private TextFieldWithBrowseButton EDAInstallPath;
+    private JTextField ProfileName;
+    private JComboBox comboBox1;
+}
