4274aa2a798635ba134861a4cd7d1483e80dea47
Chiro Liang <Chiro2001@163.com>
2022-12-27 21:02:37 +0800

    search project config

--- src/main/scala/top/criwits/scaleda/ScaledaMain.scala
+++ src/main/scala/top/criwits/scaleda/ScaledaMain.scala
@@ -1,15 +1,30 @@
 package top.criwits.scaleda
 
+import project.config.ProjectConfigLoader
 import utils.MainLogger
 
 import com.intellij.openapi.project.{DumbAware, Project}
+import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.vfs.VirtualFile
 
 class ScaledaMain extends DumbAware with StartupActivity {
   override def runActivity(project: Project) = {
     MainLogger.Log.warn("Hi there, it's scaleda! Launching...")
     // check is having project config
-    if
+    var searchedFile: Option[VirtualFile] = None
+    ProjectFileIndex.getInstance(project).iterateContent(fileOrDir => {
+      if (!fileOrDir.isDirectory && fileOrDir.getName == ProjectConfigLoader.defaultConfigFile) {
+        searchedFile = Some(fileOrDir)
+      }
+      true
+    })
+    if (searchedFile.isEmpty) {
+      MainLogger.Log.warn("there's no project config")
+    } else {
+      val f = searchedFile.get
+      MainLogger.Log.warn(s"found config: ${f}")
+    }
     MainLogger.Log.warn("Scaleda launched.")
   }
 }
