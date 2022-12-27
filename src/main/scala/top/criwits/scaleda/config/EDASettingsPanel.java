023f3261fd96bbbc701de0352254cc923fd38e20
criwits <hanswan@tom.com>
2022-12-27 23:17:34 +0800

    update

--- src/main/scala/top/criwits/scaleda/config/EDASettingsPanel.java
+++ src/main/scala/top/criwits/scaleda/config/EDASettingsPanel.java
@@ -2,13 +2,15 @@ package top.criwits.scaleda.config;
 
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
 import top.criwits.scaleda.toolchain.ToolchainProfile;
+import top.criwits.scaleda.toolchain.ToolchainType;
+import top.criwits.scaleda.toolchain.ToolchainType$;
 
 import javax.swing.*;
 
 public class EDASettingsPanel {
     private TextFieldWithBrowseButton EDAInstallPath;
-    private JTextField ProfileName;
-    private JComboBox comboBox1;
+    private JTextField profileName;
+    private JComboBox toolType;
     private JPanel mainPanel;
     private JPanel optionPanel;
 
@@ -16,6 +18,23 @@ public class EDASettingsPanel {
         return mainPanel;
     }
 
+    public void loadToolchainTypes() {
+    }
+
+    public void disableAll() {
+        profileName.setText("");
+        profileName.setEnabled(false);
+
+        toolType.setEnabled(false);
+        EDAInstallPath.setText("");
+        EDAInstallPath.setEditable(false);
+    }
+
+    public void loadProfile(ToolchainProfile profile) {
+        profileName.setText(profile.name());
+        profileName.setEnabled(true);
+
+    }
     public ToolchainProfile getProfile() {
         // TODO
         return null;
