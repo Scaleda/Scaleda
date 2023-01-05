3b30c7ebeac5988599d4fb149ed12ef16f122653
Chiro Liang <Chiro2001@163.com>
2023-01-05 08:46:00 +0800

    use blocking queue

--- src/main/scala/top/criwits/scaleda/kernel/shell/command/CommandRunner.scala
+++ src/main/scala/top/criwits/scaleda/kernel/shell/command/CommandRunner.scala
@@ -4,23 +4,23 @@ package kernel.shell.command
 import top.criwits.scaleda.kernel.utils.KernelLogger
 
 import java.io.File
-import java.util.stream.Stream
+import java.util.concurrent.LinkedBlockingQueue
 import scala.concurrent.{Future, Promise}
 import scala.sys.process._
 
-case class CommandOutputStream(returnValue: Future[Int], stdOut: Stream[String], stdErr: Stream[String])
+case class CommandOutputStream(returnValue: Future[Int], stdOut: LinkedBlockingQueue[String], stdErr: LinkedBlockingQueue[String])
 
 class CommandRunner(command: String, private var path: String = null, env: Seq[(String, String)] = Seq()) {
   if (path == null) path = System.getProperty("user.dir")
   val workingDir = new File(path)
   private val proc = Process(command, workingDir, env: _*)
-  private val returnValue = Promise[Int]
-  private val stdOut = new java.util.LinkedList[String]
-  private val stdErr = new java.util.LinkedList[String]
+  private val returnValue = Promise[Int]()
+  private val stdOut = new LinkedBlockingQueue[String]
+  private val stdErr = new LinkedBlockingQueue[String]
   private val thread = new Thread(() => {
     val exitValue = proc ! ProcessLogger(
-      out => stdOut.push(out),
-      err => stdErr.push(err),
+      out => stdOut.put(out),
+      err => stdErr.put(err),
     )
     returnValue.success(exitValue)
   })
@@ -28,7 +28,7 @@ class CommandRunner(command: String, private var path: String = null, env: Seq[(
   def run: CommandOutputStream = {
     thread.setDaemon(true)
     thread.start()
-    CommandOutputStream(returnValue.future, stdOut.stream(), stdErr.stream())
+    CommandOutputStream(returnValue.future, stdOut, stdErr)
   }
 }
 
