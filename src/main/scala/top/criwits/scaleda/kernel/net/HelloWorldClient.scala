abd44b1f5659d343fb6935db555941a8f9aeb1d4
Chiro Liang <Chiro2001@163.com>
2023-01-05 08:01:32 +0800

    add client

---
+++ src/main/scala/top/criwits/scaleda/kernel/net/HelloWorldClient.scala
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2015, Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ *    * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package top.criwits.scaleda
+package kernel.net
+
+import com.example.protos.helloworld.GreeterGrpc.GreeterBlockingStub
+import com.example.protos.helloworld.{GreeterGrpc, HelloRequest}
+import io.grpc.{ManagedChannel, ManagedChannelBuilder, StatusRuntimeException}
+
+import java.util.concurrent.TimeUnit
+import java.util.logging.{Level, Logger}
+import scala.language.existentials
+
+/**
+ * [[https://github.com/grpc/grpc-java/blob/v0.15.0/examples/src/main/java/io/grpc/examples/helloworld/HelloWorldClient.java]]
+ */
+object HelloWorldClient {
+  def apply(host: String, port: Int): HelloWorldClient = {
+    val builder = ManagedChannelBuilder.forAddress(host, port)
+    builder.usePlaintext()
+    val channel = builder.build()
+    val blockingStub = GreeterGrpc.blockingStub(channel)
+    new HelloWorldClient(channel, blockingStub)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val client = HelloWorldClient("localhost", 50051)
+    new GreeterBlockingStub()
+    try {
+      val user = args.headOption.getOrElse("world")
+      client.greet(user)
+    } finally {
+      client.shutdown()
+    }
+  }
+}
+
+class HelloWorldClient private(
+                                private val channel: ManagedChannel,
+                                private val blockingStub: GreeterBlockingStub
+                              ) {
+  private[this] val logger = Logger.getLogger(classOf[HelloWorldClient].getName)
+
+  def shutdown(): Unit = {
+    channel.shutdown.awaitTermination(5, TimeUnit.SECONDS)
+  }
+
+  /** Say hello to server. */
+  def greet(name: String): Unit = {
+    logger.info("Will try to greet " + name + " ...")
+    val request = HelloRequest(name = name)
+    try {
+      val response = blockingStub.sayHello(request)
+      logger.info("Greeting: " + response.message)
+    }
+    catch {
+      case e: StatusRuntimeException =>
+        logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus)
+    }
+  }
+}
