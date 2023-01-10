4a4a1531ce51cecbcd30523e1012231f986751be
Chiro Liang <Chiro2001@163.com>
2023-01-10 22:02:42 +0800

    update small icon

--- src/main/scala/top/criwits/scaleda/idea/utils/Icons.scala
+++ src/main/scala/top/criwits/scaleda/idea/utils/Icons.scala
@@ -5,10 +5,16 @@ import com.intellij.openapi.util.IconLoader
 
 object Icons {
   final val mainRaw = IconLoader.getIcon("/icons/icon-raw", Icons.getClass)
-  final val mainRawPlain = IconLoader.getIcon("/icons/icon-raw-plain", Icons.getClass)
+  final val mainRawPlain =
+    IconLoader.getIcon("/icons/icon-raw-plain", Icons.getClass)
   final val mainSmall = IconLoader.getIcon("/icons/icon-small", Icons.getClass)
-  final val mainWithName = IconLoader.getIcon("/icons/icon-with-name", Icons.getClass)
-  final val mainWithNamePlain = IconLoader.getIcon("/icons/icon-with-name-plain", Icons.getClass)
+  final val mainSmallDark =
+    IconLoader.getIcon("/icons/icon-small_dark", Icons.getClass)
+  final val mainWithName =
+    IconLoader.getIcon("/icons/icon-with-name", Icons.getClass)
+  final val mainWithNamePlain =
+    IconLoader.getIcon("/icons/icon-with-name-plain", Icons.getClass)
   final val verilog = IconLoader.getIcon("/icons/verilog", Icons.getClass)
-  final val toolWindow = IconLoader.getIcon("/icons/scaledaToolWindow", Icons.getClass)
+  final val toolWindow =
+    IconLoader.getIcon("/icons/scaledaToolWindow", Icons.getClass)
 }
