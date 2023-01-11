2d5d777bb41f92a80f89636aca002ad40df2083e
Chiro Liang <Chiro2001@163.com>
2023-01-12 05:54:34 +0800

    clearing verilog from tcl

--- src/main/scala/top/criwits/scaleda/tcl/TclPSIFileRoot.scala
+++ src/main/scala/top/criwits/scaleda/tcl/TclPSIFileRoot.scala
@@ -2,12 +2,10 @@ package top.criwits.scaleda
 package tcl
 
 import verilog.psi.ScopeNode
-import verilog.psi.factory.nodes.ModuleDeclarationPsiNode
 
 import com.intellij.extapi.psi.PsiFileBase
 import com.intellij.openapi.fileTypes.FileType
-import com.intellij.psi.util.PsiTreeUtil
-import com.intellij.psi.{FileViewProvider, PsiNamedElement}
+import com.intellij.psi.FileViewProvider
 
 import javax.swing.Icon
 
@@ -24,12 +22,14 @@ class TclPSIFileRoot(viewProvider: FileViewProvider)
 
   override def getContext: ScopeNode = null
 
-  def getAvailableNamedElementsScala: Array[PsiNamedElement] =
-    PsiTreeUtil
-      .findChildrenOfType(this, classOf[ModuleDeclarationPsiNode])
-      .toArray
-      .map(x => x.asInstanceOf[PsiNamedElement])
+  // def getAvailableNamedElementsScala: Array[PsiNamedElement] =
+  //   PsiTreeUtil
+  //     .findChildrenOfType(this, classOf[ModuleDeclarationPsiNode])
+  //     .toArray
+  //     .map(x => x.asInstanceOf[PsiNamedElement])
+  //
+  // override def getAvailableNamedElements =
+  //   PsiTreeUtil.findChildrenOfType(this, classOf[ModuleDeclarationPsiNode])
 
-  override def getAvailableNamedElements =
-    PsiTreeUtil.findChildrenOfType(this, classOf[ModuleDeclarationPsiNode])
+  override def getAvailableNamedElements = java.util.List.of()
 }
--- src/main/scala/top/criwits/scaleda/tcl/psi/factory/nodes/IdentifierPsiNode.scala
+++ src/main/scala/top/criwits/scaleda/tcl/psi/factory/nodes/IdentifierPsiNode.scala
@@ -1,14 +1,12 @@
 package top.criwits.scaleda
 package tcl.psi.factory.nodes
 
-import verilog.parser.VerilogLexer
-import verilog.psi.factory.VerilogPsiLeafNodeFactory
+import tcl.parser.TclParser
 
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiNameIdentifierOwner
 import com.intellij.util.IncorrectOperationException
 import org.antlr.intellij.adaptor.psi.ANTLRPsiNode
-import top.criwits.scaleda.tcl.parser.TclParser
 
 
 class IdentifierPsiNode(node: ASTNode) extends ANTLRPsiNode(node) with PsiNameIdentifierOwner {
@@ -20,5 +18,5 @@ class IdentifierPsiNode(node: ASTNode) extends ANTLRPsiNode(node) with PsiNameId
   override def getNameIdentifier = getFirstChild.getFirstChild
 
   @throws[IncorrectOperationException]
-  override def setName(s: String) = getFirstChild.replace(VerilogPsiLeafNodeFactory.create(TclParser.IDENTIFICADOR, s))
+  override def setName(s: String) = getFirstChild.replace(TclPsiLeafNodeFactory.create(TclParser.IDENTIFICADOR, s))
 }
