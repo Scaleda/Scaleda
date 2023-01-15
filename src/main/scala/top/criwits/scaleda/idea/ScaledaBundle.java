89d23a86430b501fcb625c04a1ed54adf9c30884
Chiro Liang <Chiro2001@163.com>
2023-01-16 02:12:51 +0800

    i18n: add todo

--- src/main/scala/top/criwits/scaleda/idea/ScaledaBundle.java
+++ src/main/scala/top/criwits/scaleda/idea/ScaledaBundle.java
@@ -8,6 +8,9 @@ import java.util.function.Supplier;
 
 public final class ScaledaBundle extends DynamicBundle {
     @NonNls private static final String BUNDLE = "bundles.ScaledaBundle";
+    // TODO: support i18n
+    // note that if we replace INSTANCE with BUNDLE_ZH on launch, shown language may change
+    @NonNls private static final String BUNDLE_ZH = "bundles.ScaledaBundle_zh";
     private static final ScaledaBundle INSTANCE = new ScaledaBundle();
 
     private ScaledaBundle() { super(BUNDLE); }
