33de98e7b5a5d8d044d3a0d7a637f29a84b8cf7d
Chiro Liang <Chiro2001@163.com>
2023-01-16 11:32:05 +0800

    auto expand all after creation

--- src/main/scala/top/criwits/scaleda/idea/ScaledaMain.scala
+++ src/main/scala/top/criwits/scaleda/idea/ScaledaMain.scala
@@ -55,7 +55,6 @@ class ScaledaMain extends StartupActivity {
       // builder.stripeTitle = ScaledaBundle.message("tasks.tool.window.title")
       val toolWindow = toolWindowManager.registerToolWindow(builder.build())
     })
-    toolWindowManager.getToolWindow(ScaledaRunWindowFactory.WINDOW_ID).getContentManager
 
     MainLogger.info("Scaleda launched.")
 
