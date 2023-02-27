123b5624146bf61c0dd687f0469f1748e40e3853
Chiro Liang <Chiro2001@163.com>
2023-02-27 17:41:37 +0800

    testing rvcd, err: UNIMPLEMENTED

--- src/main/scala/top/criwits/scaleda/kernel/rvcd/RvcdClient.scala
+++ src/main/scala/top/criwits/scaleda/kernel/rvcd/RvcdClient.scala
@@ -6,10 +6,16 @@ import io.grpc.ManagedChannelBuilder
 import scala.language.existentials
 
 object RvcdClient {
-  def apply(host: String, port: Int) = {
+  private final val DEFAULT_PORT = 5411
+  def apply(host: String, port: Int = DEFAULT_PORT) = {
     val builder = ManagedChannelBuilder.forAddress(host, port)
     builder.usePlaintext()
     val channel = builder.build()
-    RvcdClientGrpc.blockingStub(channel)
+    RvcdRpcGrpc.blockingStub(channel)
   }
+}
+
+object RvcdTest extends App {
+  private val stub = RvcdClient("localhost")
+  stub.openFile(RvcdOpenFile.of("/home/chiro/programs/rvcd/data/testbench0.vcd"))
 }
