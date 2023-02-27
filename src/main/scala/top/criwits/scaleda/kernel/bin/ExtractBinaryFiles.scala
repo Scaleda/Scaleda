61350288416d3415a8f5787708f701f2cc2890f0
Chiro Liang <Chiro2001@163.com>
2023-02-27 10:33:03 +0800

    fix and chmod

--- src/main/scala/top/criwits/scaleda/kernel/bin/ExtractBinaryFiles.scala
+++ src/main/scala/top/criwits/scaleda/kernel/bin/ExtractBinaryFiles.scala
@@ -1,35 +1,36 @@
 package top.criwits.scaleda
 package kernel.bin
 
-import kernel.utils.{KernelLogger, Paths}
+import kernel.utils.{KernelLogger, OS, Paths}
 
-import java.io.File
-import java.nio.file.{Files, StandardCopyOption}
+import java.io.{File, FileOutputStream}
 import scala.collection.mutable.ArrayBuffer
 
 object ExtractBinaryFiles {
   private val targetDirectory = new File(Paths.getConfigDir, "bin")
   private val binaryList      = Array("rvcd", "rvcd.exe")
   private val resourcePath    = "bin/"
-  def run(): Seq[String] = {
+  def run(force: Boolean = false): Seq[String] = {
     val copiedList = ArrayBuffer[String]()
     if (!targetDirectory.exists()) targetDirectory.mkdirs();
     for (binary <- binaryList) {
       val resource = resourcePath + binary
       try {
-        val target = new File(targetDirectory, binary)
-        // TODO: check working in jar
-        val resourceUrl = getClass.getClassLoader.getResource(resource)
-        val f           = new File(resourceUrl.getFile)
-        if (!(f.exists() && f.isFile && f.length() > 0))
-          KernelLogger.error("error archive file:", f)
-        if (target.length() != f.length()) {
-          Files.copy(f.toPath, target.toPath, StandardCopyOption.REPLACE_EXISTING)
+        val target         = new File(targetDirectory, binary)
+        val resourceStream = getClass.getClassLoader.getResourceAsStream(resource)
+        if (force || !target.exists()) {
+          resourceStream.transferTo(new FileOutputStream(target))
+          if (!OS.isWindows) {
+            import sys.process._
+            s"chmod +x ${target.getAbsolutePath}".!
+          }
           copiedList += binary
         }
       } catch {
-        // case _e: NullPointerException => {}
-        case e: Throwable             => KernelLogger.warn("Cannot extract binary file", binary, ": ", e)
+        case _e: NullPointerException => {}
+        case e: Throwable =>
+          KernelLogger.warn("Cannot extract binary file", binary, ": ", e)
+          e.printStackTrace()
       }
     }
     if (copiedList.nonEmpty) KernelLogger.info("copied binaries:", copiedList.mkString(", "))
