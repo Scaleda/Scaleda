d2535ab5b4e0459110e31d0a0ab7b8c433c1cf0a
Chiro Liang <Chiro2001@163.com>
2023-02-27 09:48:48 +0800

    ExtractBinaryFiles

--- src/main/scala/top/criwits/scaleda/kernel/bin/ExtractBinaryFiles.scala
+++ src/main/scala/top/criwits/scaleda/kernel/bin/ExtractBinaryFiles.scala
@@ -1,25 +1,34 @@
 package top.criwits.scaleda
 package kernel.bin
 
-import kernel.utils.Paths
+import kernel.utils.{KernelLogger, Paths}
 
 import java.io.File
-import java.nio.file.Files
+import java.nio.file.{Files, StandardCopyOption}
 
 object ExtractBinaryFiles {
   private val targetDirectory = new File(Paths.getConfigDir, "bin")
   private val binaryList      = Array("rvcd", "rvcd.exe")
   private val resourcePath    = "bin/"
   def run(): Unit = {
+    if (!targetDirectory.exists()) targetDirectory.mkdirs();
     for (binary <- binaryList) {
       val resource = resourcePath + binary
       try {
         val target = new File(targetDirectory, binary)
-        val inputStream = getClass.getClassLoader.getResourceAsStream(resource)
-        Files.copy(inputStream, target.toPath)
+        // TODO: check working in jar
+        val resourceUrl = getClass.getClassLoader.getResource(resource)
+        val f = new File(resourceUrl.getFile)
+        if (target.length() != f.length())
+          Files.copy(f.toPath, target.toPath, StandardCopyOption.REPLACE_EXISTING)
       } catch {
-        case _e: _ => {}
+        case _e: NullPointerException => {}
+        case e: Throwable             => KernelLogger.warn("Cannot extract binary file", binary, ": ", e)
       }
     }
   }
 }
+
+// object ExtractTest extends App {
+//   ExtractBinaryFiles.run()
+// }
