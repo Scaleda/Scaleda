bb45ba22f3c8e7b21bbea090acbdc30104e8e6f7
Chiro Liang <Chiro2001@163.com>
2023-03-01 22:56:31 +0800

    todo: code navigation

--- src/main/scala/top/criwits/scaleda/idea/utils/RpcService.scala
+++ src/main/scala/top/criwits/scaleda/idea/utils/RpcService.scala
@@ -2,8 +2,13 @@ package top.criwits.scaleda
 package idea.utils
 
 import kernel.utils.KernelLogger
+import verilog.VerilogPSIFileRoot
 
+import com.intellij.codeInsight.navigation.NavigationUtil
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vcs.LocalFilePath
+import com.intellij.psi.PsiManager
 import io.grpc.{ManagedChannelBuilder, Server, ServerBuilder}
 import scaleda.scaleda.{ScaledaEmpty, ScaledaGotoSource, ScaledaRpcGrpc}
 
@@ -12,7 +17,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{ExecutionContext, Future}
 import scala.language.existentials
 
-class ScaledaRpcServerImpl extends ScaledaRpcGrpc.ScaledaRpc {
+class ScaledaRpcServerImpl(project: () => Project) extends ScaledaRpcGrpc.ScaledaRpc {
   override def ping(request: ScaledaEmpty): Future[ScaledaEmpty] = async {
     KernelLogger.info("grpc ping")
     ScaledaEmpty.of()
@@ -20,12 +25,34 @@ class ScaledaRpcServerImpl extends ScaledaRpcGrpc.ScaledaRpc {
 
   override def gotoSource(request: ScaledaGotoSource): Future[ScaledaEmpty] = async {
     KernelLogger.info(s"grpc gotoSource($request)")
+    // TODO: follows not work. do navigation
+    inReadAction {
+      val psi = PsiManager
+        .getInstance(project())
+        .findFile(new LocalFilePath(request.file, false).getVirtualFile)
+        .asInstanceOf[VerilogPSIFileRoot]
+      KernelLogger.warn(s"psi: $psi, canNavigate: ${psi.canNavigate}, canNavigateToSource: ${psi.canNavigateToSource}")
+
+      NavigationUtil.activateFileWithPsiElement(psi, true)
+      // psi.navigate(true)
+
+      // val reqService: NavigationRequest = NavigationService.instance().sourceNavigationRequest(new LocalFilePath(request.file, false).getVirtualFile, 0)
+      // KernelLogger.warn(s"reqService: $reqService")
+
+      // new NavigatorWithinProject(project(), new java.util.HashMap[String, String](), locationToOffset)
+      // SymbolNavigationService.getInstance().psiFileNavigationTarget(psi)
+      // SymbolNavigationService.getInstance().psiElementNavigationTarget(psi)
+    }
     ScaledaEmpty.of()
   }
 }
 
 class RpcService extends Disposable {
   private final val DEFAULT_PORT = 4151
+
+  private var myProject: Project = _
+
+  def setProject(project: Project): Unit = myProject = project
 
   var stop                   = false
   var server: Option[Server] = None
@@ -36,22 +63,23 @@ class RpcService extends Disposable {
         val method   = provider.getClass.getDeclaredMethod("builderForPort", Integer.TYPE)
         method.setAccessible(true)
         val builder = method.invoke(provider, DEFAULT_PORT).asInstanceOf[ServerBuilder[_]]
-        builder.addService(ScaledaRpcGrpc.bindService(new ScaledaRpcServerImpl, ExecutionContext.global))
+        builder.addService(
+          ScaledaRpcGrpc.bindService(new ScaledaRpcServerImpl(() => myProject), ExecutionContext.global)
+        )
         MainLogger.info("scaleda grpc server serve at port", DEFAULT_PORT)
         server = Some(builder.build().start())
         server.get.awaitTermination()
       } catch {
-        case _e: Throwable => {
+        case _e: Throwable =>
           MainLogger.warn("trying", _e)
           _e.printStackTrace()
           Thread.sleep(3000)
-        }
       }
     }
   }).start()
 
   override def dispose() = {
-    stop = true;
+    stop = true
     server.foreach(s => s.shutdown())
     server = None
   }
@@ -70,7 +98,7 @@ object RpcServiceTest extends App {
   } else {
     ServerBuilder.forPort(DEFAULT_PORT)
   }
-  builder.addService(ScaledaRpcGrpc.bindService(new ScaledaRpcServerImpl, ExecutionContext.global))
+  builder.addService(ScaledaRpcGrpc.bindService(new ScaledaRpcServerImpl(() => null), ExecutionContext.global))
   KernelLogger.info("before start()")
   val server = builder.build().start()
   val serverThread = new Thread(() => {
@@ -81,7 +109,7 @@ object RpcServiceTest extends App {
   private val clientBuilder = ManagedChannelBuilder.forAddress("127.0.0.1", DEFAULT_PORT)
   clientBuilder.usePlaintext()
   val channel = clientBuilder.build()
-  val stub = ScaledaRpcGrpc.blockingStub(channel)
+  val stub    = ScaledaRpcGrpc.blockingStub(channel)
   stub.ping(ScaledaEmpty.of())
   channel.shutdownNow()
 
@@ -89,13 +117,12 @@ object RpcServiceTest extends App {
   KernelLogger.info("done")
 }
 
-
 object RpcServiceClientTest extends App {
   private final val DEFAULT_PORT = 4151
-    private val clientBuilder = ManagedChannelBuilder.forAddress("127.0.0.1", DEFAULT_PORT)
+  private val clientBuilder      = ManagedChannelBuilder.forAddress("127.0.0.1", DEFAULT_PORT)
   clientBuilder.usePlaintext()
   val channel = clientBuilder.build()
-  val stub = ScaledaRpcGrpc.blockingStub(channel)
+  val stub    = ScaledaRpcGrpc.blockingStub(channel)
   stub.ping(ScaledaEmpty.of())
   channel.shutdownNow()
   KernelLogger.info("done")
