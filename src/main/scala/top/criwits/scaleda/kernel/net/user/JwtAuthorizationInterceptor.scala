c8d8d2a31a11d4e83342d297f49e6bd1a62538f6
Chiro Liang <Chiro2001@163.com>
2023-03-08 11:40:08 +0800

    clean

--- src/main/scala/top/criwits/scaleda/kernel/net/user/JwtAuthorizationInterceptor.scala
+++ src/main/scala/top/criwits/scaleda/kernel/net/user/JwtAuthorizationInterceptor.scala
@@ -27,14 +27,10 @@ class JwtAuthorizationInterceptor(passServices: HashSet[String] = HashSet[String
 
     var status = Status.OK
     if (value == null) status = Status.UNAUTHENTICATED.withDescription("Authorization token is missing")
-    // else if (!value.startsWith(Constant.BEARER_TYPE)) status = Status.UNAUTHENTICATED.withDescription("Unknown authorization type")
     else {
       var claims: Option[Token] = None
-      // remove authorization type prefix
-      val token = value
+      val token                 = value
       try {
-        // verify token signature and parse claims
-        // claims = parser.parseClaimsJws(token)
         val db    = new ScaledaDatabase
         val found = db.findToken(token)
         if (found.isEmpty) {
@@ -53,9 +49,7 @@ class JwtAuthorizationInterceptor(passServices: HashSet[String] = HashSet[String
     }
 
     serverCall.close(status, new Metadata())
-    new ServerCall.Listener[ReqT]() {
-      // noop
-    }
+    new ServerCall.Listener[ReqT]() {}
   }
 }
 
