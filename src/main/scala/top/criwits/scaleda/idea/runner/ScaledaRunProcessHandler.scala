03b67b909f5ccedfb9caed865b5f8ece74074097
Chiro Liang <Chiro2001@163.com>
2023-03-15 00:58:29 +0800

    runtime context: file replace in logger

--- src/main/scala/top/criwits/scaleda/idea/runner/ScaledaRunProcessHandler.scala
+++ src/main/scala/top/criwits/scaleda/idea/runner/ScaledaRunProcessHandler.scala
@@ -3,9 +3,10 @@ package idea.runner
 
 import idea.ScaledaBundle
 import idea.utils.{MainLogger, OutputLogger}
+import idea.windows.tool.message.ScaledaMessageRenderer
 import kernel.shell.ScaledaRunHandler
 import kernel.shell.command.CommandDeps
-import kernel.utils.BasicLogger
+import kernel.utils.{BasicLogger, FileReplaceContext}
 
 import com.intellij.execution.process.ProcessHandler
 import com.intellij.openapi.project.Project
@@ -14,16 +15,16 @@ import java.io.OutputStream
 import scala.collection.mutable.ArrayBuffer
 
 /** Handle a task process locally or remotely
- *
+  *
   * @param logger            using logger
   * @param rt                Runtime of this run
   * @param invokeAfterFinish will emit with [[ScaledaRuntime]] after `finishedAll`
   */
 class ScaledaRunProcessHandler(
-                                project: Project,
-                                logger: BasicLogger,
-                                rt: ScaledaRuntime,
-                                invokeAfterFinish: (ScaledaRuntime, Seq[Int], Boolean, Boolean) => Unit
+    project: Project,
+    logger: BasicLogger,
+    rt: ScaledaRuntime,
+    invokeAfterFinish: (ScaledaRuntime, Seq[Int], Boolean, Boolean) => Unit
 ) extends ProcessHandler
     with ScaledaRunHandler {
   // Set terminating <- `true` to invoke stopping
@@ -67,14 +68,47 @@ class ScaledaRunProcessHandler(
 
   override def onStepDescription(data: String): Unit = logger.debug(data)
 
+  private def doContextReplace(src: String): String = {
+    var now = src
+    rt.context
+      .get("replaceFiles")
+      .map(_.asInstanceOf[Seq[FileReplaceContext]])
+      .foreach(_.foreach(replace => {
+        val m            = ScaledaMessageRenderer.fileOptionalLineNumberPattern.matcher(now)
+        val stringBuffer = new StringBuffer()
+        while (m.find()) {
+          val line = Option(m.group(m.groupCount()))
+          if (m.group(1).equals(replace.to.getAbsolutePath)) {
+            m.appendReplacement(
+              stringBuffer,
+              replace.from.getAbsolutePath + line
+                .map(line => {
+                  val number =
+                    (try Some(Integer.parseInt(line))
+                    catch {
+                      case e: NumberFormatException => None
+                    }).map(_ - replace.lineOffset)
+                  number.map(":" + _.toString).getOrElse("")
+                })
+                .getOrElse("")
+            )
+          } else {
+            m.appendReplacement(stringBuffer, m.group())
+          }
+        }
+        now = m.appendTail(stringBuffer).toString
+      }))
+    now
+  }
+
   override def onStdout(data: String): Unit = {
-    logger.info(data)
-    outputLogger.info(data)
+    logger.info(doContextReplace(data))
+    outputLogger.info(doContextReplace(data))
   }
 
   override def onStderr(data: String): Unit = {
-    logger.warn(data)
-    outputLogger.warn(data)
+    logger.warn(doContextReplace(data))
+    outputLogger.warn(doContextReplace(data))
   }
 
   override def onReturn(returnValue: Int, finishedAll: Boolean, meetErrors: Boolean): Unit = {
@@ -82,8 +116,7 @@ class ScaledaRunProcessHandler(
     if (meetErrors) {
       logger.warn(msg)
       outputLogger.warn(msg)
-    }
-    else {
+    } else {
       logger.debug(msg)
       outputLogger.debug(msg)
     }
