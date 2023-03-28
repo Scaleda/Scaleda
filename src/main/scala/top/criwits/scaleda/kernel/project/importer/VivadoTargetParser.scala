73e40fcd66b17c52d4b9f319fc3cb00233113c11
Chiro Liang <Chiro2001@163.com>
2023-03-28 19:08:53 +0800

    parse vivado target

--- src/main/scala/top/criwits/scaleda/kernel/project/importer/VivadoTargetParser.scala
+++ src/main/scala/top/criwits/scaleda/kernel/project/importer/VivadoTargetParser.scala
@@ -4,6 +4,7 @@ package kernel.project.importer
 import kernel.project.config.TargetConfig
 import kernel.project.detect.VivadoProjectConfig
 import kernel.toolchain.impl.Vivado
+import kernel.utils.ImplicitPathReplace
 import kernel.utils.serialise.XMLHelper
 
 import java.io.File
@@ -11,11 +12,17 @@ import java.io.File
 class VivadoTargetParser extends BasicTargetParser {
   override def parseAsTarget(path: File): TargetConfig = {
     val projectFile = path.listFiles((file, s) => s.endsWith(".xpr")).head
+    val projectName = projectFile.getName.split("\\.").head
     val o           = XMLHelper(projectFile, classOf[VivadoProjectConfig])
+    // $PSRCDIR/sim_1 => <projectBase>/<projectName>.srcs/sim_1
+    val replace = new ImplicitPathReplace("", "", Some("(\\$PSRCDIR[/\\\\]?)"), Seq(s"$projectName.srcs/"))
     require(o.Version == "7")
     val target = TargetConfig(
       name = "Vivado",
-      toolchain = Vivado.internalID
+      toolchain = Vivado.internalID,
+      // add source directory
+      sources = o.fileSets.filter(_.Type == "DesignSrcs").map(_.RelSrcDir).map(replace.doRegexReplace),
+      tests = o.fileSets.filter(_.Type == "SimulationSrcs").map(_.RelSrcDir).map(replace.doRegexReplace)
     )
     target
   }
--- src/test/scala/top/criwits/scaleda/kernel/ProjectParserTester.scala
+++ src/test/scala/top/criwits/scaleda/kernel/ProjectParserTester.scala
@@ -4,19 +4,31 @@ package kernel
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 import top.criwits.scaleda.kernel.project.detect.VivadoProjectConfig
+import top.criwits.scaleda.kernel.project.importer.VivadoTargetParser
 import top.criwits.scaleda.kernel.utils.serialise.XMLHelper
 
+import java.io.File
 import scala.io.Source
 
 class ProjectParserTester extends AnyFlatSpec with should.Matchers {
   behavior of "ProjectParser"
 
-  it should "test Vivado project parser" in {
+  it should "test Vivado project config parser" in {
     val fileContent = Source
       .fromInputStream(getClass.getClassLoader.getResourceAsStream("vivado/project/ip_tests.xml"))
       .getLines()
       .mkString
     val obj = XMLHelper(fileContent, classOf[VivadoProjectConfig])
     println(s"obj: ${obj}")
+  }
+
+  it should "test vivado parse config and directory" in {
+    val path = "/home/chiro/Vivado/Tests/ip_tests/"
+    val file = new File(path)
+    if (file.exists()) {
+      val parser = new VivadoTargetParser
+      val target = parser.parseAsTarget(file)
+      println(target)
+    }
   }
 }
