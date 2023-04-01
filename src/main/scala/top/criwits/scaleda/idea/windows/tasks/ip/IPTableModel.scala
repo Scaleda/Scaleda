da1a66703feedf7b76f33a021c9fd0e6807fc8e4
criwits <hanswan@tom.com>
2023-04-01 19:41:19 +0800

    ip manager

--- src/main/scala/top/criwits/scaleda/idea/windows/tasks/ip/IPTableModel.scala
+++ src/main/scala/top/criwits/scaleda/idea/windows/tasks/ip/IPTableModel.scala
@@ -11,26 +11,28 @@ import java.lang.{Boolean => JBoolean}
 final class IPTableModel
     extends ListTableModel[IP](
       Array(
-      new ColumnInfo[IP, String](ScaledaBundle.message("windows.ip.name")) {
-        override def valueOf(item: IP): String = item.config.exports.get.name
+        new ColumnInfo[IP, String](ScaledaBundle.message("windows.ip.name")) {
+          override def valueOf(item: IP): String = item.config.exports.get.name
 
-        override def getPreferredStringValue: String = "SuperIP" // Why do we need this?
-      },
-      new ColumnInfo[IP, String](ScaledaBundle.message("windows.ip.vendor")) {
-        override def valueOf(item: IP): String       = "Generic" // TODO
-        override def getPreferredStringValue: String = "Generic"
-      },
-      new ColumnInfo[IP, String](ScaledaBundle.message("windows.ip.repository")) {
-        override def valueOf(item: IP): String       = item.path
-        override def getPreferredStringValue: String = "/"
-      }) ++
-      Toolchain.toolchains
-        .map(toolchain => {
-          new IPTableModel.BooleanColumnInfo(toolchain._2._1) {
-            override def valueOf(item: IP): JBoolean = ??? // TODO
-          }
-        })
-        .toArray: _*
+          override def getPreferredStringValue: String = "SuperIP" // Why do we need this?
+        },
+        new ColumnInfo[IP, String](ScaledaBundle.message("windows.ip.vendor")) {
+          override def valueOf(item: IP): String =
+            Toolchain.toolchains.get(item.config.exports.get.vendor).map(_._1).getOrElse("Genric")
+          override def getPreferredStringValue: String = "Generic"
+        },
+        new ColumnInfo[IP, String](ScaledaBundle.message("windows.ip.repository")) {
+          override def valueOf(item: IP): String       = item.path
+          override def getPreferredStringValue: String = "/"
+        }
+      ) ++
+        Toolchain.toolchains
+          .map(toolchain => {
+            new IPTableModel.BooleanColumnInfo(toolchain._2._1) {
+              override def valueOf(item: IP): JBoolean = item.config.exports.get.getSupports.contains(toolchain._1)
+            }
+          })
+          .toArray: _*
     )
 
 object IPTableModel {
