e73789238e657ad3cb1d249741b0dbb1ec164232
Chiro Liang <Chiro2001@163.com>
2023-04-11 09:39:11 +0800

    fix: when no module instance name

--- src/main/scala/top/criwits/scaleda/verilog/annotation/instantiation/ModulePortConnectionAnnotator.scala
+++ src/main/scala/top/criwits/scaleda/verilog/annotation/instantiation/ModulePortConnectionAnnotator.scala
@@ -2,13 +2,11 @@ package top.criwits.scaleda
 package verilog.annotation.instantiation
 
 import idea.{ScaledaBundle => SB}
-import verilog.psi.nodes.instantiation.{ModuleInstancePsiNode, ModuleInstantiationPsiNode, NameOfInstancePsiNode, NamedPortConnectionPsiNode, OrderedPortConnectionPsiNode}
+import verilog.psi.nodes.instantiation.{ModuleInstancePsiNode, ModuleInstantiationPsiNode}
 import verilog.psi.nodes.module.ModuleDeclarationPsiNode
-import verilog.psi.nodes.signal.PortDeclarationPsiNode
 
 import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.lang.annotation.{AnnotationHolder, Annotator, HighlightSeverity}
-import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
 
@@ -19,21 +17,28 @@ class ModulePortConnectionAnnotator extends Annotator {
     val instance = element.asInstanceOf[ModuleInstantiationPsiNode]
     if (instance == null) return
 
-    val reference     = instance.getReference
-    val result        = reference.resolve
+    val reference = instance.getReference
+    val result    = reference.resolve
 
     if (result != null) {
       // Module is valid, so should check
-      val module = result.asInstanceOf[ModuleDeclarationPsiNode]
+      val module  = result.asInstanceOf[ModuleDeclarationPsiNode]
       val connMap = instance.getConnectMap
 
       if (connMap.exists(_._2.isEmpty)) {
-        // has empty connection
-        holder.newAnnotation(HighlightSeverity.WARNING, SB.message("annotation.not.connected.port", connMap.filter(_._2.isEmpty).map(_._1.getName).mkString(", ")))
-          .range(PsiTreeUtil.getChildOfType(instance, classOf[ModuleInstancePsiNode]).getTextRange)
-          .withFix(new ModulePortConnectionIntent(instance, connMap))
-          .highlightType(ProblemHighlightType.WARNING)
-          .create()
+        val moduleInstancePsiNode = PsiTreeUtil.getChildOfType(instance, classOf[ModuleInstancePsiNode])
+        if (moduleInstancePsiNode != null) {
+          // has empty connection
+          holder
+            .newAnnotation(
+              HighlightSeverity.WARNING,
+              SB.message("annotation.not.connected.port", connMap.filter(_._2.isEmpty).map(_._1.getName).mkString(", "))
+            )
+            .range(moduleInstancePsiNode.getTextRange)
+            .withFix(new ModulePortConnectionIntent(instance, connMap))
+            .highlightType(ProblemHighlightType.WARNING)
+            .create()
+        }
       }
 
     }
